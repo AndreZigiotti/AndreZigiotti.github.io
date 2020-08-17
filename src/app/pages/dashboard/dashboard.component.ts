d34368b5675ddc4b7bbbdf34d765f6e4cea8acc5
André Luiz Gagliardi Zigiotti <algazigiotti@hotmail.com>
2020-08-17 12:08:06 -0300

    button to add tasks + fixing some styles

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { StorageService } from '../../shared/services/storage/storage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,18 +8,23 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  todo = [
-    'Task 1',
-    'Task 2',
-    'Task 3',
-    'Task 4'
-  ]
-
+  todo = []
   done = []
 
-  constructor() { }
+  constructor(private storageService: StorageService) { }
 
   ngOnInit(): void {
+    let tasks = this.storageService.getItem('tasks')
+    if(tasks) {
+      const {todo, done} = JSON.parse(tasks)
+      this.todo = todo
+      this.done = done
+    }
+  }
+
+  createTask() {
+    this.todo.push(`Task ${this.todo.length + this.done.length + 1}`)
+    this.saveTasks()
   }
 
   drop(event: CdkDragDrop<string[]>) {
@@ -28,7 +34,10 @@ export class DashboardComponent implements OnInit {
       transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
 
-    console.log(event)
+    this.saveTasks()
   }
 
+  saveTasks() {
+    this.storageService.setItem('tasks', JSON.stringify({todo: this.todo, done: this.done}))
+  }
 }
